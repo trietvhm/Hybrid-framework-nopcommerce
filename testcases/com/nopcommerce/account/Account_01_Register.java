97ce2d5233e15383945608f0d768e92cffcaabb7
Triet Vu <trietvhm@gmail.com>
2024-03-28 23:22:59 +0700

    Update

---
+++ testcases/com/nopcommerce/account/Account_01_Register.java
@@ -0,0 +1,40 @@
+package com.nopcommerce.account;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Account_01_Register {
+    @BeforeClass
+    public void beforeClass() {
+    }
+
+    @Test
+    public void Register_01_Empty_Data() {
+
+    }
+
+    @Test
+    public void Register_02_Invalid_Email() {
+
+    }
+
+    @Test
+    public void Register_03_Invalid_Password() {
+
+    }
+
+    @Test
+    public void Register_04_Incorrect_Confirm_Password() {
+
+    }
+    @Test
+    public void Register_05_Success() {
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+    }
+
+}
